06e3589f8859c80cca929c1df5a5be7d62ee2633
unknown <lena.kilian91@gmail.com>
2025-02-24 23:07:53 +0800

    feat: transition of loader class into typescript

---
+++ src/components/additional.ts
@@ -0,0 +1,54 @@
+interface IOptions {
+    apiKey?: string;
+}
+
+enum ResponseStatus {
+    Unauthorized = 401,
+    NotFound  = 404
+}
+
+enum Methods {
+    GET = 'GET',
+    POST = 'POST',
+    PUT = 'PUT',
+    DELETE = 'DELETE'
+}
+
+interface INews<T> {
+    source: {
+        id: number;
+        name: T;
+    };
+    author: T;
+    title: T;
+    description: T;
+    url: T;
+    urlToImage: T;
+    publishedAt: T;
+    content: T;
+}
+
+interface ISources<T> {
+    category: T;
+    country: T;
+    description: T;
+    id: T;
+    language: T;
+    name: T;
+    url: T;
+}
+
+interface INewsData {
+    articles?: ReadonlyArray<INews<string>[]>;
+}
+
+interface ISourcesData {
+    articles?: ReadonlyArray<ISources<string[]>>;
+}
+
+type TMethod = Methods.GET | Methods.POST | Methods.PUT | Methods.DELETE
+
+type TCallback = (data: INewsData | ISourcesData) => void;
+
+
+export {IOptions, ResponseStatus, Methods, TCallback, TMethod}
---
+++ src/components/controller/loader.ts
@@ -0,0 +1,54 @@
+import { IOptions, ResponseStatus, Methods, TMethod, TCallback } from '../additional'
+
+class Loader {
+
+    private baseLink: string;
+    private options:  IOptions;
+
+
+
+    constructor(baseLink: string, options: IOptions) {
+        this.baseLink = baseLink;
+        this.options = options;
+    }
+
+    public getResp(
+        { endpoint, options = {} }: {endpoint: string, options?: IOptions},
+        callback = (): void => {
+            console.error('No callback for GET response');
+        }
+    ): void {
+        this.load(Methods.GET, endpoint, callback, options);
+    }
+
+    private errorHandler(res: Response): Response {
+        if (!res.ok) {
+            if (res.status === ResponseStatus.Unauthorized || res.status === ResponseStatus.NotFound)
+                console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+            throw Error(res.statusText);
+        }
+
+        return res;
+    }
+
+    private makeUrl(options: IOptions, endpoint: string): string {
+        const urlOptions: IOptions = { ...this.options, ...options };
+        let url = `${this.baseLink}${endpoint}?`;
+
+        Object.keys(urlOptions).forEach((key) => {
+            url += `${key}=${urlOptions[key as keyof object]}&`;
+        });
+
+        return url.slice(0, -1);
+    }
+
+    protected load(method: TMethod, endpoint: string, callback: TCallback, options: IOptions = {}): void {
+        fetch(this.makeUrl(options, endpoint), { method })
+            .then(this.errorHandler)
+            .then((res) => res.json())
+            .then((data) => callback(data))
+            .catch((err) => console.error(err));
+    }
+}
+
+export default Loader;
